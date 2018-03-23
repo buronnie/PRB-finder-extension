25a9382a7e94f250281840ce903932d53aef5b9d
kai.guo <kai.guo@servicenow.com>
2018-03-22 20:02:11 -0700

    kg: update icon and support enter keypress

--- popup.js
+++ popup.js
@@ -1,8 +1,15 @@
 const searchBtn = document.querySelector('#search-button');
+const input = document.querySelector('#input');
 if (searchBtn) {
   searchBtn.addEventListener('click', () => {
-    const ticketNumber = document.querySelector('#ticket-number').value;
+    const ticketNumber = input.value;
     const url = `https://hi.service-now.com/text_search_exact_match.do?sysparm_search=${ticketNumber}`;
     chrome.tabs.create({ url });
   });
 }
+
+input.addEventListener('keyup', (ev) => {
+  if (ev.keyCode === 13) {
+    searchBtn.click();
+  }
+});
